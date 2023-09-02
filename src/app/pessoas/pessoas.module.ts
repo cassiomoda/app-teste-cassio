cef3992f3e8374bb270f8fb9ad6b215b4710602b
Cassio Moda <cassiomoda@gmail.com>
2023-09-02 09:22:37 -0300

    iniciando a implementação da lista de pessoas

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 import { PessoasRoutingModule } from './pessoas-routing.module';
 import { PessoasComponent } from './pessoas/pessoas.component';
-
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { PessoasComponent } from './pessoas/pessoas.component';
   ],
   imports: [
     CommonModule,
-    PessoasRoutingModule
+    PessoasRoutingModule,
+    MatTableModule
   ]
 })
 export class PessoasModule { }
--- src/app/pessoas/pessoas/pessoas.component.ts
+++ src/app/pessoas/pessoas/pessoas.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from '../model/pessoa';
 
 @Component({
   selector: 'app-pessoas',
   templateUrl: './pessoas.component.html',
   styleUrls: ['./pessoas.component.css']
 })
-export class PessoasComponent {
+export class PessoasComponent implements OnInit {
 
+  pessoas: Pessoa[];
+  displayedColumns: string[];
+
+  constructor() {
+    this.pessoas = [
+      {
+        _id: 1,
+        nome: 'Milena Analu Pires',
+        cpf: '775.256.099-50',
+        rg: '44.084.541-5',
+        data_nasc: '23\/05\/1964',
+        sexo: 'Feminino',
+        mae: 'Isadora Marli',
+        pai: 'Noah Severino César Pires',
+        email: 'mmilenaanalupires@keffin.com.br',
+        cep: '39801-678',
+        endereco: 'Rua Kurt W. Rothe',
+        numero: '675',
+        bairro: 'Castro Pires',
+        cidade: 'Teófilo Otoni',
+        estado: 'MG',
+        telefone_fixo: '(33) 3611-4613',
+        celular: '(33) 98481-0191',
+        altura: 1.53,
+        peso: 80,
+        tipo_sanguineo: "O-"
+      },
+      {
+        _id: 2,
+        nome: 'Marcos Vinicius Kevin Samuel Santos',
+        cpf: '024.934.035-68',
+        rg: '10.701.456-7',
+        data_nasc: '07\/09\/1992',
+        sexo: 'Masculino',
+        mae: 'Isabella Andrea',
+        pai: 'Lorenzo Marcos André Santos',
+        email: 'marcosviniciuskevinsamuelsantos_@dhl.com',
+        cep: '49091-043',
+        endereco: 'Rua Manoel de Oliveira França',
+        numero: '634',
+        bairro: 'Jardim Centenário',
+        cidade: 'Aracaju',
+        estado: 'SE',
+        telefone_fixo: '(79) 2686-2642',
+        celular: '(79) 99666-0063',
+        altura: 1.92,
+        peso: 95,
+        tipo_sanguineo: 'O-'
+      }
+    ];
+    this.displayedColumns = ['nome', 'cpf', 'data_nasc', 'sexo', 'email', 'cidade', 'estado', 'tipo_sanguineo'];
+  }
+
+  ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+  }
 }
